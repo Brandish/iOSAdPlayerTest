c6b2e6672ba1e7d60f3abb49dde85049d41fb1c4
Faridh Mendoza <faridh@intangible.mx>
2016-07-08 14:37:10 -0400

    Reverts URL Request to JWPlayer

--- BrandishJWP/BrandishJWP/ViewController.m
+++ BrandishJWP/BrandishJWP/ViewController.m
@@ -20,11 +20,6 @@
 //    webView.frame = CGRectMake(0, 20, self.view.frame.size.width, self.view.frame.size.height - 20);
 //    webView.delegate = self;
 //    webView.backgroundColor = [UIColor darkGrayColor];
-//    
-//    NSString *urlString = @"http://brandish.com/jwplayer/index.html";
-//    NSURL *url = [NSURL URLWithString:urlString];
-//    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
-//    [webView loadRequest:urlRequest];
     
     UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0,100.0f, 290.0, 180.0)];
     
@@ -32,13 +27,22 @@
     
     webView.delegate = self;
     
+    // Waywire player Sample load
 //    NSString *html = @"<html><head><script src='//decor.waywire.com/javascript/waywire_ref.js'></script></head><body><iframe src='https://player.waywire.com/?id=";
 //    NSString *id_video = @"QKJG7C112NY3BLGV";
 //    NSString *html2 =@"&chromeless=true&autoplay=true&force_autoplay=true' frameborder='0' width='290' height='170' allowfullscreen></iframe></body></html>";
 
-    NSString *html = @"<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/xKcqJTrpACo?autoplay=true\" frameborder=\"0\" allowfullscreen> </iframe>";
+    // Direct YouTube Video Load
+//    NSString *html = @"<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/xKcqJTrpACo?autoplay=true\" frameborder=\"0\" allowfullscreen> </iframe>";
 //    [webView loadHTMLString:[NSString stringWithFormat:@"%@%@%@", html,id_video,html2] baseURL:nil];
-    [webView loadHTMLString:[NSString stringWithFormat:@"%@", html] baseURL:nil];
+//    [webView loadHTMLString:[NSString stringWithFormat:@"%@", html] baseURL:nil];
+    
+    // JWPlayer
+    NSString *urlString = @"http://brandish.com/jwplayer/index.html";
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:urlRequest];
+    
     [self.view addSubview:webView];
 }
 
