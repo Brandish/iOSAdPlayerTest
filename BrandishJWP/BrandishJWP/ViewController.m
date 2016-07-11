9acf1a0d9aabfe0b07753f9820c618caffefd7c1
Faridh Mendoza <faridh@intangible.mx>
2016-07-11 17:43:18 -0400

    Caching UIWebView

--- BrandishJWP/BrandishJWP/ViewController.m
+++ BrandishJWP/BrandishJWP/ViewController.m
@@ -21,27 +21,35 @@
 //    webView.delegate = self;
 //    webView.backgroundColor = [UIColor darkGrayColor];
     
-    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0,100.0f, 290.0, 180.0)];
-    
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 32.0, 320.0, 180.0)];
+    webView.mediaPlaybackRequiresUserAction = NO;
     webView.scrollView.scrollEnabled = NO;
     
     webView.delegate = self;
     
     // Waywire player Sample load
 //    NSString *html = @"<html><head><script src='//decor.waywire.com/javascript/waywire_ref.js'></script></head><body><iframe src='https://player.waywire.com/?id=";
-//    NSString *id_video = @"QKJG7C112NY3BLGV";
-//    NSString *html2 =@"&chromeless=true&autoplay=true&force_autoplay=true' frameborder='0' width='290' height='170' allowfullscreen></iframe></body></html>";
+//    NSString *id_video = @"MLFV0C0J4TNW23KZ";
+//    NSString *html2 =@"&force_autoplay=true' frameborder='0' width='290' height='170' allowfullscreen></iframe></body></html>";
+//    [webView loadHTMLString:[NSString stringWithFormat:@"%@%@%@", html,id_video,html2] baseURL:nil];
 
     // Direct YouTube Video Load
 //    NSString *html = @"<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/xKcqJTrpACo?autoplay=true\" frameborder=\"0\" allowfullscreen> </iframe>";
-//    [webView loadHTMLString:[NSString stringWithFormat:@"%@%@%@", html,id_video,html2] baseURL:nil];
 //    [webView loadHTMLString:[NSString stringWithFormat:@"%@", html] baseURL:nil];
     
-    // JWPlayer
-    NSString *urlString = @"http://brandish.com/jwplayer/index.html";
-    NSURL *url = [NSURL URLWithString:urlString];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
-    [webView loadRequest:urlRequest];
+//    // JWPlayer
+    __block NSString *urlString = @"https://player.waywire.com/?id=SBTM9X1KFBMP2PZZ&autoplay=true&force_autoplay=true";
+    __block NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:3600];
+    [webView loadRequest:request];
+    
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        urlString = @"https://player.waywire.com/?id=MLFV0C0J4TNW23KZ&autoplay=true&force_autoplay=true";
+//        url = [NSURL URLWithString:urlString];
+//        NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:3600];
+//        [webView loadRequest:request];
+//    });
+
     [self.view addSubview:webView];
     
 }
---
+++ BrandishJWP/Pods/Target Support Files/Pods-BrandishJWPUITests/Pods-BrandishJWPUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BrandishJWPUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BrandishJWPUITests
+@end
